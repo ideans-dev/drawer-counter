c1a46c33cf6b08fdb26b0ae38ce6c5e31c666396
Ian Deans <iandeans062@gmail.com>
2023-11-16 14:06:04 -0800

    update to v1.1

---
+++ src/components/DrawerCounter/CashCounter/CashCounter.tsx
@@ -0,0 +1,129 @@
+"use client"
+import React from 'react';
+import styles from './CashCounter.module.css';
+import { Container, Input, Segment } from 'semantic-ui-react';
+
+type IState = {
+  "1": string,
+  "5": string,
+  "10": string,
+  "20": string,
+  "50": string,
+  "100": string,
+}
+
+
+const INITIAL_STATE: IState = {
+  "1": "",
+  "5": "",
+  "10": "",
+  "20": "",
+  "50": "",
+  "100": "",
+}
+
+function reducer( state: IState, action: any ): IState {
+  switch ( action.type ) {
+    case '1': {
+      const newState: IState = { ...state, "1": action.payload };
+      return newState;
+    }
+    case '5': {
+      const newState: IState = { ...state, "5": action.payload };
+      return newState;
+    }
+    case '10': {
+      const newState: IState = { ...state, "10": action.payload };
+      return newState;
+    }
+    case '20': {
+      const newState: IState = { ...state, "20": action.payload };
+      return newState;
+    }
+    case '50': {
+      const newState: IState = { ...state, "50": action.payload };
+      return newState;
+    }
+    case '100': {
+      const newState: IState = { ...state, "100": action.payload };
+      return newState;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+const InitialSumsState = {
+  "1": "",
+  "5": "",
+  "10": "",
+  "20": "",
+  "50": "",
+  "100": "",
+}
+
+export default function CashCounter( { setTotal, total }: any ) {
+  const [ state, dispatch ] = React.useReducer( reducer, INITIAL_STATE );
+
+  React.useEffect( () => {
+    let denominationCounts = Object.values( state );
+    let denominations = Object.keys( state );
+    let denominationSums = denominations.map((denom, index) => {
+      return Number(denom) * Number(denominationCounts[index]);
+    })
+
+    let totalSum = denominationSums.reduce((totalSum, currentSum) => totalSum += currentSum, 0);
+    if (Number(totalSum)) {
+      setTotal(totalSum);
+    }
+
+  }, [ state ] )
+
+
+  return (
+    <Container className={ styles.container }>
+      <Segment.Group className={ styles.segmentGroup }>
+        <DenominationRow dispatchFn={dispatch} denomination="1" value={state["1"]} />
+        <DenominationRow dispatchFn={dispatch} denomination="5" value={state["5"]} />
+        <DenominationRow dispatchFn={dispatch} denomination="10" value={state["10"]} />
+        <DenominationRow dispatchFn={dispatch} denomination="20" value={state["20"]} />
+        <DenominationRow dispatchFn={dispatch} denomination="50" value={state["50"]} />
+        <DenominationRow dispatchFn={dispatch} denomination="100" value={state["100"]} />
+        <Segment className={ styles.row }>
+          <div className={styles.inputContainer}>
+            <span>Grand Total</span>
+          </div>
+          <div className={styles.sumContainer}>${ total }</div>
+        </Segment>
+      </Segment.Group>
+    </Container>
+  );
+}
+
+function DenominationRow({ dispatchFn, denomination, value }: any) {
+  const [ sum, setSum ] = React.useState(0);
+
+  React.useEffect(() => {
+    let newSum = Number(value) * Number(denomination);
+    setSum(newSum);
+  }, [value])
+
+  return (
+    <Segment className={ styles.row }>
+          <div className={ styles.inputContainer }>
+            <Input
+              className={ styles.input }
+              label={denomination}
+              placeholder="# of Bills"
+              onChange={ ( event ) => dispatchFn( { type: denomination, payload: event.target.value } ) }
+              value={value}
+            />
+          </div>
+          <div className={ styles.sumContainer }>
+            ${sum}
+          </div>
+        </Segment>
+  )
+}
--- src/components/DrawerCounter/DrawCounter.tsx
+++ src/components/DrawerCounter/DrawCounter.tsx
@@ -1,127 +1,54 @@
 "use client";
 import React from 'react';
 import styles from './DrawerCounter.module.css';
-import Row from '@/components/DenominationRow/Row';
+import CashCounter from './CashCounter/CashCounter';
 
-type IState = {
-  "1": number,
-  "5": number,
-  "10": number,
-  "20": number,
-  "50": number,
-  "100": number,
-}
-
-
-const INITIAL_STATE: IState = {
-  "1": 0,
-  "5": 0,
-  "10": 0,
-  "20": 0,
-  "50": 0,
-  "100": 0,
-}
-
-function reducer(state: IState, action: any): IState {
-  switch (action.type) {
-    case 'update 1s': {
-      const newState: IState = { ...state, "1": action.payload };
-      return newState;
-    }
-    case 'update 5s': {
-      const newState: IState = { ...state, "5": action.payload };
-      return newState;
-    }
-    case 'update 10s': {
-      const newState: IState = { ...state, "10": action.payload };
-      return newState;
-    }
-    case 'update 20s': {
-      const newState: IState = { ...state, "20": action.payload };
-      return newState;
-    }
-    case 'update 50s': {
-      const newState: IState = { ...state, "50": action.payload };
-      return newState;
-    }
-    case 'update 100s': {
-      const newState: IState = { ...state, "100": action.payload };
-      return newState;
-    }
-
-    default: {
-      return state;
-    }
-  }
-}
-
-function calculateTotal(denominationValues: number[]): number {
-  return denominationValues.reduce((total, currentValue) => {
-    total += currentValue;
-    return total;
-  }, 0)
-}
 
 export default function DrawerCounter() {
 
-  const [state, dispatch] = React.useReducer(reducer, INITIAL_STATE);
-  const [total, setTotal] = React.useState(0);
-  const [expectedTotal, setExpectedTotal] = React.useState();
-  const [difference, setDifference] = React.useState(0);
+  const [ total, setTotal ] = React.useState( 0 );
+  const [ drawer1Expected, setDrawer1Expected ] = React.useState();
+  const [ drawer2Expected, setDrawer2Expected ] = React.useState();
+  const [ totalExpected, setTotalExpected ] = React.useState( 0 );
 
+  React.useEffect( () => {
 
-  React.useEffect(() => {
-    let denominationValues = Object.values(state);
-    let newTotal = calculateTotal(denominationValues);
-    setTotal(newTotal)
-  }, [state])
+    let newTotalExpected = Number( drawer1Expected )
+      + Number( drawer2Expected );
 
-  React.useEffect(() => {
-    let newDifference = Number(total) - Number(expectedTotal);
-    if ( Number(newDifference ) ) {
-      setDifference( newDifference );
+    if ( !isNaN( newTotalExpected ) ) {
+      setTotalExpected( newTotalExpected )
     }
-  }, [total, expectedTotal])
+  }, [ total ] )
 
-  const handleChangeExpected = ({target: { value }}: any) => {
-    setExpectedTotal( value );
+  const handleChangeExpected = ( { target: { value } }: any ) => {
+    setTotalExpected( value );
   }
 
 
   return (
-    <div className={styles.container}>
-
-      <Row dispatchFn={dispatch} denomination={1} />
-      <Row dispatchFn={dispatch} denomination={5} />
-      <Row dispatchFn={dispatch} denomination={10} />
-      <Row dispatchFn={dispatch} denomination={20} />
-      <Row dispatchFn={dispatch} denomination={50} />
-      <Row dispatchFn={dispatch} denomination={100} />
-
-
-      <div className={styles.totalRow}>
-        <span>Total</span>
-        <span> </span>
-        <span className={styles.totalValue}>
-          ${total}
-        </span>
+    <div className={ styles.container }>
+      <div className={styles.cashcounterArea}>
+        <CashCounter setTotal={ setTotal } total={ total } />
       </div>
 
-      <div className={styles.differenceRow}>
-        <span>Expect</span>
-        <span className={styles.inputContainer}>
-        <input
-          className={styles.expectedInput}
-          type="number"
-          onChange={ handleChangeExpected }
-          value={expectedTotal}
-          />
-        </span>
-        <span className={styles.difference}>
-          ${difference}
-        </span>
+      <div className={styles.drawersArea}>
+        <DrawerData />
       </div>
 
+      <div className={styles.breakdownArea}>
+
+      </div>
+
+    </div>
+  )
+}
+
+function DrawerData() {
+
+  return (
+    <div className={styles.drawerCounter}>
+      <span>TEST</span>
     </div>
   )
 }
